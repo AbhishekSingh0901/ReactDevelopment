3a235d000bbed91dcf7c869537b79738ff85561f
Abhishek Singh <as9284608@gmail.com>
2023-04-11 14:47:15 +0530

    routing_init

--- 02-practice-project/src/components/User/AddUser.js
+++ 02-practice-project/src/components/User/AddUser.js
@@ -1,39 +1,40 @@
-import React, { useRef, useState } from "react";
-import Card from "../UI/Card";
-import Button from "../UI/Button";
-import ErrorModal from "../UI/ErrorModal";
-import Wrapper from "../Helpers/Wrapper";
+import React, { useRef, useState } from "react"
+import Card from "../UI/Card"
+import Button from "../UI/Button"
+import ErrorModal from "../UI/ErrorModal"
+import Wrapper from "../Helpers/Wrapper"
+
 const AddUser = (props) => {
-  const nameInputRef = useRef();
-  const ageInputRef = useRef();
-  const [error, setError] = useState();
+  const nameInputRef = useRef()
+  const ageInputRef = useRef()
+  const [error, setError] = useState()
 
   const addUserHandler = (e) => {
-    e.preventDefault();
-    const enteredName = nameInputRef.current.value;
-    const enteredUserAge = ageInputRef.current.value;
+    e.preventDefault()
+    const enteredName = nameInputRef.current.value
+    const enteredUserAge = ageInputRef.current.value
     if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid name and age (non-empty values)",
-      });
-      return;
+      })
+      return
     }
     if (+enteredUserAge < 1) {
       setError({
         title: "Invalid Age",
         message: "Please enter a valid age ( > 0 )",
-      });
-      return;
+      })
+      return
     }
-    props.onAddUser(enteredName, enteredUserAge);
-    nameInputRef.current.value = "";
-    ageInputRef.current.value = "";
-  };
+    props.onAddUser(enteredName, enteredUserAge)
+    nameInputRef.current.value = ""
+    ageInputRef.current.value = ""
+  }
 
   const errorHandler = () => {
-    setError(null);
-  };
+    setError(null)
+  }
   return (
     <Wrapper>
       {error && (
@@ -53,6 +54,6 @@ const AddUser = (props) => {
         </form>
       </Card>
     </Wrapper>
-  );
-};
-export default AddUser;
+  )
+}
+export default AddUser
---
+++ learning_routing_alma/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css"
+import Content from "./components/Contents"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Sidebar from "./components/Sidebar"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <div className="row">
+        <Content />
+        <Sidebar />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
