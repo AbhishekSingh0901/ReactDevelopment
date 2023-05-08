db33d966acf4cdb1a6087bc779e0034d18986aa4
Abhishek Singh <as9284608@gmail.com>
2023-05-08 15:16:35 +0530

    learning about React memo and useCallback hook

--- 05-ReactWorking/src/App.js
+++ 05-ReactWorking/src/App.js
@@ -1,13 +1,22 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState, useCallback } from "react"
+import Button from "./components/UI/Button/Button"
+import "./App.css"
+import DemoOutput from "./components/Demo/DemoOutput"
 
 function App() {
+  const [showPara, setShowPara] = useState(false)
+
+  const toggleParaHandler = useCallback(() => {
+    setShowPara((showPara) => !showPara)
+  }, [])
+  console.log("App running")
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showPara} />
+      <Button onClick={toggleParaHandler}>Toggle Paragraph</Button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ 05-ReactWorking/src/components/Demo/DemoOutput.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+const DemoOutput = (props) => {
+  console.log("Demo running")
+  return <h2>{props.show ? "Nice to meet you" : "rattatatata"}</h2>
+}
+
+export default React.memo(DemoOutput)
